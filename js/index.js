182833b6db4e0c49905d7e9645d3d38b54fab4eb
vcarames <109115310+vcarames@users.noreply.github.com>
2022-12-04 21:55:09 -0800

    Email Validation

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("#form");
+const email = document.querySelector(".email");
+const mainPage = document.querySelector(".main-page");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (regEx.test(email.value)) {
+    mainPage.classList.add("hide");
+    document.getElementById("confirmation-container").removeAttribute("hidden");
+  }
+});
